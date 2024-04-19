7e2eb0f960e331a6fcc078210e9edce2ffb3d63f
Facundo Palombo facundo.palombo@gmail.com <facundo.palombo@gmail.com>
2024-04-19 15:48:34 +0200

    feat: add dockerfiles for playwright and node

--- playwright.config.js
+++ playwright.config.js
@@ -21,7 +21,7 @@ module.exports = defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: 1,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: "html",
+  reporter: "list",
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     // I dont want to load the scripts unnecesary because the only that i will get is the google maps path
